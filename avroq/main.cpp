3facd1e220d78496741d0e8e0614275063ddf13d
Galanin Mikhail <galanin@corp.mail.ru>
2014-11-09 00:12:17 +0300

    Initial Commit

---
+++ avroq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  avroq
+//
+//  Created by Mikhail Galanin on 09/11/14.
+//  Copyright (c) 2014 Mikhail Galanin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
