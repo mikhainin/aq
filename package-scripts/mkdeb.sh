a082aed7c574f10ee02b95a968bb1dda6a0cd8bb
Mikhail Galanin <mikhail.galanin@yahoo.com>
2015-07-20 16:25:14 +0300

    Small changes

--- package-scripts/mkdeb.sh
+++ package-scripts/mkdeb.sh
@@ -3,8 +3,8 @@
 sudo apt-get install fakeroot-ng git-buildpackage debhelper build-essential
 BOOST_ROOT=/home/mgalanin/boost_1_57_0/ DH_VERBOSE=1 dpkg-buildpackage
 
-cp ~/aq-*.deb /var/www/debs/amd64/
+cp ~/aq[-_]*.deb /var/www/debs/amd64/
 
-cd /var/www/debs/
+cd /var/www/debs/amd64
 
-dpkg-scanpackages amd64 | gzip -9c > amd64/Packages.gz
+dpkg-scanpackages . | gzip -9c > Packages.gz
