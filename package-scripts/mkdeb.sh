bf8ca3aeb5194b0111838009adef9168c4614bc8
Mikhail Galanin <mikhail.galanin@yahoo.com>
2015-07-20 14:16:40 +0300

    local repo

--- package-scripts/mkdeb.sh
+++ package-scripts/mkdeb.sh
@@ -3,3 +3,8 @@
 sudo apt-get install fakeroot-ng git-buildpackage debhelper build-essential
 BOOST_ROOT=/home/mgalanin/boost_1_57_0/ DH_VERBOSE=1 dpkg-buildpackage
 
+cp ~/aq-*.deb /var/www/debs/amd64/
+
+cd /var/www/debs/
+
+dpkg-scanpackages amd64 | gzip -9c > amd64/Packages.gz
