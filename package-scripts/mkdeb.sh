78cc086a1ac8b55e00cec76df1eae773dcb9c921
Galanin Mikhail <mikhail.galanin@yahoo.com>
2015-07-24 09:03:20 +0300

    Package script
    Changelog

--- package-scripts/mkdeb.sh
+++ package-scripts/mkdeb.sh
@@ -3,6 +3,9 @@
 sudo apt-get install fakeroot-ng git-buildpackage debhelper build-essential
 BOOST_ROOT=/home/mgalanin/boost_1_57_0/ DH_VERBOSE=1 dpkg-buildpackage
 
+install debian/aq/usr/bin/aq /var/www/utils/aq/aq
+install debian/aq-dbg/usr/lib/debug/usr/bin/aq /var/www/utils/aq/aq.debug
+
 cp ~/aq[-_]*.deb /var/www/debs/amd64/
 
 cd /var/www/debs/amd64
